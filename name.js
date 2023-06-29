ceca330358c28db6da38a746fa3333fb9b98313b
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-06-29 17:53:07 +0200

    Summary

--- name.js
+++ name.js
@@ -1,6 +1,27 @@
 function change_name(){
     const name = new URLSearchParams(window.location.search).get("name");
+    const message = new URLSearchParams(window.location.search).get("text");
+    const link = new URLSearchParams(window.location.search).get("link");
 
     text = document.getElementById('birthday-text')
-    text.innerHTML = "Happy birthday!<br>" + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    text.innerHTML = message + "<br>" + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    console.log(String(link) == "https://example.com");
+    if (String(link) == "https://example.com"){
+        document.getElementById("rainbow-button").style.display = "none";
+    }
+    else {
+        if (!link.startsWith("https://")) {
+            corrected_link = "https://" + link;
+            document.getElementById("rainbow-button").style.display = "block";
+            document.getElementById("rainbow-button").href = corrected_link;
+        }
+        else {
+            document.getElementById("rainbow-button").style.display = "block";
+            document.getElementById("rainbow-button").href = link;
+        }
+    }
+
+    const urlWithoutParams = window.location.href.split('?')[0];
+    history.replaceState({}, document.title, urlWithoutParams);
 }
+  
